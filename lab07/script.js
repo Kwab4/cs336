97bd9b5925072ecdccb5724b29910ea1f17bc9e5
Kwab4 <32646585+Kwab4@users.noreply.github.com>
2018-10-20 09:14:47 -0400

    Update script.js

--- lab07/script.js
+++ lab07/script.js
@@ -1,35 +1,24 @@
-$(document).ready(function () {
+$( function() {
+    $( ".widget input[type=submit], .widget a, .widget button" ).button();
+    $( "button, input, a" ).click( function( event ) {
+      $("<em>", {html: "no data yet..."}).appendTo("body");
+           
 
-    $(function () {
-        $(".widget button").button();
-        $("button").click(function (event) {
-            $("<em>", {
-                html: "no data yet..."
-            }).appendTo("body");
-        });
-    });
-
-
-    $("button").click(
-        function () {
-            event.preventDefault();
-            let jsPromise = Promise.resolve($.ajax({
-                url: "/fetch",
-                type: "GET",
-                data: {
-                    name: "lab07"
-                }
-            }));
-            jsPromise.then(function (result) {
-                console.log('AJAX request succeeded...');
-                $("#fetch").next("div").html("<p>" + result.content + "</p>");
-            }, function (xhr) {
-                console.log('AJAX request failed...');
-                $("#fetch").next("div").html("<p>  AJAX request failed: " + xhr.statusText + "</p>");
-            });
-
+         let jsPromise = Promise.resolve($.ajax({
+        url: "/hello",
+        type: "GET",
+        data: {
+            name: "Hello World! This is lab7!"
         }
-
-    );
-
-});
+    }));
+    jsPromise.then(function (result) {
+        console.log('AJAX request succeeded...');
+       // $("<em>", {html: result.content}).appendTo("body");
+        $( "em" ).html(result.content );
+    }, function (xhr) {
+        console.log('AJAX request failed...');
+        $( "em" ).html('AJAX request failed...');
+    })
+      event.preventDefault();
+    } );
+  } );
