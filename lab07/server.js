baea88b133d7a9db994161e36bb39b4603b0a98c
Kwab4 <32646585+Kwab4@users.noreply.github.com>
2018-10-20 09:17:29 -0400

    Add files via upload

---
+++ lab07/server.js
@@ -0,0 +1,25 @@
+
+const express = require("express")
+const app = express();
+const http_status = require("http-status-codes");
+const bodyParser = require("body-parser")
+
+const HOST = "localhost";
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res) {
+    res.send("Hello, Lab 7!");
+});
+
+app.get("/hello", function(req, res) {
+    res.send({"content" : req.query.name});
+});
+
+app.listen(PORT, HOST, () => {
+    console.log("listening on " + HOST + ":" + PORT + "...");
+});
